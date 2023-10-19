7ca277c7cfb3812c786c70e2a697bd5a56873d8c
YopterMatbEro <yopter2013@gmail.com>
2023-10-19 16:40:54 +0300

    2.2_m2m_relations

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,8 +1,33 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
+from .models import Article, Scope, Tag
 
-from .models import Article
+
+class ScopeInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        main_scope_count = 0
+        for form in self.forms:
+            if form.cleaned_data.get('is_main'):
+                main_scope_count += 1
+        if main_scope_count > 1:
+            raise ValidationError('Основным может быть только один раздел')
+        elif main_scope_count == 0:
+            raise ValidationError('Укажите основной раздел')
+        return super().clean()
+
+
+class ScopeInline(admin.TabularInline):
+    model = Scope
+    formset = ScopeInlineFormset
+    extra = 1
 
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
+    inlines = [ScopeInline]
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
     pass
